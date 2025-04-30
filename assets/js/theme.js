9ba6480fa92600bfcb4654932a140a413cd42807
Debliqui <debliqui.valentin@outlook.com>
2025-04-30 11:43:22 -0400

    Improved theme.js and modified banner image

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,9 +1,13 @@
-const button = document.querySelector("[data-theme-toggle]")
+const toggleTheme = document.querySelector("[data-theme-toggle]")
 const themeIcon = document.querySelector("[data-theme-icon]")
 const localStorageTheme = localStorage.getItem("theme")
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)")
 const backgroundBannerSrc = document.querySelector("[data-background-src]")
+const backgroundBannerSrcset = document.querySelector(
+  "[data-background-srcset]"
+)
 
+// Initialize string according to local storage and user browser preferences.
 function calculateSettingAsThemeString({
   localStorageTheme,
   systemSettingDark,
@@ -19,9 +23,10 @@ function calculateSettingAsThemeString({
   return "light"
 }
 
-function updateButton({ buttonEl, isDark }) {
+// Update theme switch attributes
+function updateToggleTheme({ switchBtn, isDark }) {
   const newCta = isDark ? "Passer en mode claire" : "Passer en mode sombre"
-  buttonEl.setAttribute("aria-label", newCta)
+  switchBtn.setAttribute("aria-label", newCta)
   themeIcon.setAttribute(
     "class",
     isDark ? "fa-solid fa-moon" : "fa-solid fa-sun"
@@ -29,38 +34,64 @@ function updateButton({ buttonEl, isDark }) {
   themeIcon.setAttribute("data-theme-icon", isDark ? "moon" : "sun")
 }
 
+// Update theme string in html header
 function updateThemeOnHtmlEl({ theme }) {
   document.querySelector("html").setAttribute("data-theme", theme)
 }
 
+// Update banner image to match color
 function updateBackgroundBannerSrc(currentThemeSetting) {
   if (currentThemeSetting === "dark") {
     backgroundBannerSrc.setAttribute(
       "src",
-      "./assets/image/background-dark.webp"
+      "./assets/image/background-dark-366.webp"
+    )
+    backgroundBannerSrcset.setAttribute(
+      "srcset",
+      "./assets/image/background-dark-366.webp 399w, ./assets/image/background-dark-735.webp 767w, ./assets/image/background-dark-975.webp 1023w, ./assets/image/background-dark-1280.webp 1439px"
     )
   } else {
     backgroundBannerSrc.setAttribute(
       "src",
-      "./assets/image/background-light.webp"
+      "./assets/image/background-light-366.webp"
+    )
+    backgroundBannerSrcset.setAttribute(
+      "srcset",
+      "./assets/image/background-light-366.webp 399w, ./assets/image/background-light-735.webp 767w, ./assets/image/background-light-975.webp 1023w, ./assets/image/background-light-1280.webp 1439px"
     )
   }
 }
 
+// Initial mode status
 let currentThemeSetting = calculateSettingAsThemeString({
   localStorageTheme,
   systemSettingDark,
 })
 
-updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" })
+updateToggleTheme({
+  switchBtn: toggleTheme,
+  isDark: currentThemeSetting === "dark",
+})
 updateThemeOnHtmlEl({ theme: currentThemeSetting })
 updateBackgroundBannerSrc(currentThemeSetting)
 
-button.addEventListener("click", (event) => {
+// Manual theme change
+toggleTheme.addEventListener("click", (event) => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark"
 
   localStorage.setItem("theme", newTheme)
-  updateButton({ buttonEl: button, isDark: newTheme === "dark" })
+  updateToggleTheme({ switchBtn: toggleTheme, isDark: newTheme === "dark" })
+  updateThemeOnHtmlEl({ theme: newTheme })
+  updateBackgroundBannerSrc(newTheme)
+
+  currentThemeSetting = newTheme
+})
+
+// Changing the theme via browser preferences
+systemSettingDark.addEventListener("change", (event) => {
+  const newTheme = event.matches ? "dark" : "light"
+  localStorage.setItem("theme", newTheme)
+  updateToggleTheme({ switchBtn: toggleTheme, isDark: newTheme === "dark" })
   updateThemeOnHtmlEl({ theme: newTheme })
   updateBackgroundBannerSrc(newTheme)
 
